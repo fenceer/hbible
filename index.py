4cafef18cc4262acc0f726a1a760b1059e724fdd
winT60 <winT60@zobei.com>
2013-01-10 18:21:53 +0800

    wsgi

--- index.py
+++ index.py
@@ -32,6 +32,8 @@ app.add_processor(appinit.my_processor)
 app.internalerror = appinit.internalerror
 app.notfound = appinit.notfound
 
+application = app.wsgifunc()
+
 if __name__ == "__main__":
     web.wsgi.runwsgi = lambda func, addr = None: web.wsgi.runfcgi(func, addr)
     app.run()
