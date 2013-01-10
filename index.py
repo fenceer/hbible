5a33f5714b9570e323d44db7c8bddc010292c808
fenceer <fenceer@zobei.com>
2013-01-10 19:03:50 +0800

    index.py

--- index.py
+++ index.py
@@ -19,13 +19,13 @@ web.config.debug = False
 #web.config.session_parameters.timeout = 6000 #100 Minutes
 pool = redis.ConnectionPool(host='127.0.0.1', port=6379, db=3)
 web.config.redis = redis.StrictRedis(connection_pool=pool)
-web.config.mongo = pymongo.Connection('127.0.0.1', 27017)
-web.config.mongo.admin.authenticate('zobei.mdb', 'mdb27017')
+web.config.mongo = pymongo.Connection('127.4.24.129', 27017)
+web.config.mongo.admin.authenticate('admin', 'ukCMnbuzFBq8')
 web.config.db = web.config.mongo['hbible']
 
 app = web.application(urls.urls, globals())
-#web.config._session = web.session.Session(app, web.session.DiskStore('sessions'), {'count': 0})
-web.config._session = web.session.Session(app, rediswebpy.RedisStore(), initializer={'count': 0})
+web.config._session = web.session.Session(app, web.session.DiskStore('sessions'), {'count': 0})
+#web.config._session = web.session.Session(app, rediswebpy.RedisStore(), initializer={'count': 0})
 
 import appinit
 app.add_processor(appinit.my_processor)
